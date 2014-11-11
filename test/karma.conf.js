99321ed9d96cd86699a00c15d23e7f7f909a7ae6
Sean <tymon148@gmail.com>
2014-11-11 13:16:46 +0000

    testing sauce

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,12 +1,44 @@
 module.exports = function (config) {
+
+	// if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+	// 	console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+	// 	process.exit(1);
+	// }
+
+	var customLaunchers = {
+		sl_chrome: {
+			base: 'SauceLabs',
+			browserName: 'chrome',
+			platform: 'Windows 7',
+			version: '35'
+		},
+		sl_firefox: {
+			base: 'SauceLabs',
+			browserName: 'firefox',
+			version: '30'
+		},
+		sl_ios_safari: {
+			base: 'SauceLabs',
+			browserName: 'iphone',
+			platform: 'OS X 10.9',
+			version: '7.1'
+		},
+		sl_ie_11: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 8.1',
+			version: '11'
+		}
+	};
+
 	config.set({
-		basePath : '',
-		autoWatch : true,
+		basePath: '',
+		autoWatch: true,
 		frameworks: ['jasmine'],
-		browsers : ['PhantomJS'],
-		plugins : [
+		plugins: [
 			'karma-spec-reporter',
 			'karma-phantomjs-launcher',
+			'karma-sauce-launcher',
 			'karma-jasmine',
 			'karma-coverage',
 			'karma-notify-reporter'
@@ -14,15 +46,22 @@ module.exports = function (config) {
 		preprocessors: {
 			'../src/*.js': ['coverage']
     	},
-		reporters : ['spec', 'coverage', 'notify'],
+
+    	sauceLabs: {
+	        testName: 'angular-locker unit tests'
+	    },
+	    customLaunchers: customLaunchers,
+	    browsers: ['PhantomJS'].concat(Object.keys(customLaunchers)),
+		reporters: ['spec', 'coverage', 'notify', 'saucelabs'],
+		singleRun: true,
+
 		coverageReporter: {
-			type : 'lcov',
-			dir : 'coverage/',
+			type: 'lcov',
+			dir: 'coverage/',
 			subdir: function(browser) {
 				// return browser.toLowerCase().split(/[ /-]/)[0];
 				return 'lcov';
 			}
-		},
-
+		}
 	});
 };
