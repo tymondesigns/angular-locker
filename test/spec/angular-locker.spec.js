6769672aae3a6f578ee139a721f76017f1176847
Sean <tymon148@gmail.com>
2014-11-20 23:07:17 +0000

    dummy

--- test/spec/angular-locker.spec.js
+++ test/spec/angular-locker.spec.js
@@ -356,7 +356,7 @@ describe('angular-locker', function () {
 
 				expect( locker.get('something') ).not.toBeDefined();
 				expect( locker.get('anotherThing') ).not.toBeDefined();
-				expect( locker.get('lorem') ).not.toBeDefined();
+				expect( locker.get('lorem') ).toBeTruhty();
 			}));
 
 			it('should remove multiple items from locker by passing an array', inject(function () {
