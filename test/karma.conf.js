6c747c3dd09548ce3715be496763c9200fc83317
Sean <tymon148@gmail.com>
2014-11-20 21:10:25 +0000

    removing node 0.10 and some sauce tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,23 +12,23 @@ module.exports = function (config) {
 			platform: 'Windows 7',
 			version: '35'
 		},
-		sl_firefox: {
-			base: 'SauceLabs',
-			browserName: 'firefox',
-			version: '30'
-		},
+		// sl_firefox: {
+		// 	base: 'SauceLabs',
+		// 	browserName: 'firefox',
+		// 	version: '30'
+		// },
 		sl_ios_safari: {
 			base: 'SauceLabs',
 			browserName: 'iphone',
 			platform: 'OS X 10.9',
 			version: '7.1'
 		},
-		sl_ie_11: {
-			base: 'SauceLabs',
-			browserName: 'internet explorer',
-			platform: 'Windows 8.1',
-			version: '11'
-		}
+		// sl_ie_11: {
+		// 	base: 'SauceLabs',
+		// 	browserName: 'internet explorer',
+		// 	platform: 'Windows 8.1',
+		// 	version: '11'
+		// }
 	};
 
 	config.set({
