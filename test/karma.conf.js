acef88ae15ea6475641db734867cc2cd118b32c5
Sean <tymon148@gmail.com>
2015-09-17 20:06:26 +0100

    testing sauce labs again

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,54 +6,55 @@ module.exports = function (config) {
     // }
 
     var customLaunchers = {
-        // sl_chrome: {
-        //  base: 'SauceLabs',
-        //  browserName: 'chrome',
-        //  platform: 'Windows 7',
-        //  version: '38'
-        // },
-        // sl_firefox: {
-        //  base: 'SauceLabs',
-        //  browserName: 'firefox',
-        //  version: '33',
-        //  platform: 'Windows 7'
-        // },
-        // sl_ios_safari: {
-        //  base: 'SauceLabs',
-        //  browserName: 'iphone',
-        //  platform: 'OS X 10.9',
-        //  version: '7.1'
-        // },
-        // sl_mac_safari: {
-        //  base: 'SauceLabs',
-        //  browserName: 'safari',
-        //  platform: 'OS X 10.10',
-        //  version: '8'
-        // },
-        // sl_ie_11: {
-        //  base: 'SauceLabs',
-        //  browserName: 'internet explorer',
-        //  platform: 'Windows 8.1',
-        //  version: '11'
-        // },
-        // sl_ie_9: {
-        //  base: 'SauceLabs',
-        //  browserName: 'internet explorer',
-        //  platform: 'Windows 7',
-        //  version: '9'
-        // },
-        // sl_opera_12: {
-        //  base: 'SauceLabs',
-        //  browserName: 'opera',
-        //  platform: 'Windows 7',
-        //  version: '12'
-        // }
+        sl_chrome: {
+         base: 'SauceLabs',
+         browserName: 'chrome',
+         platform: 'Windows 7',
+         version: '38'
+        },
+        sl_firefox: {
+         base: 'SauceLabs',
+         browserName: 'firefox',
+         version: '33',
+         platform: 'Windows 7'
+        },
+        sl_ios_safari: {
+         base: 'SauceLabs',
+         browserName: 'iphone',
+         platform: 'OS X 10.9',
+         version: '7.1'
+        },
+        sl_mac_safari: {
+         base: 'SauceLabs',
+         browserName: 'safari',
+         platform: 'OS X 10.10',
+         version: '8'
+        },
+        sl_ie_11: {
+         base: 'SauceLabs',
+         browserName: 'internet explorer',
+         platform: 'Windows 8.1',
+         version: '11'
+        },
+        sl_ie_9: {
+         base: 'SauceLabs',
+         browserName: 'internet explorer',
+         platform: 'Windows 7',
+         version: '9'
+        },
+        sl_opera_12: {
+         base: 'SauceLabs',
+         browserName: 'opera',
+         platform: 'Windows 7',
+         version: '12'
+        }
     };
 
     config.set({
         basePath: '',
         autoWatch: true,
         frameworks: ['jasmine'],
+        // logLevel: config.LOG_DEBUG,
         preprocessors: {
             '../src/*.js': ['coverage'],
             '../test/**/*.js': ['babel']
